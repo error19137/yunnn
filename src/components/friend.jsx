9076e1f067ac8c9e530e3ef2ccdc49538a3bcd59
error19137 <thomas.cheng0217@gmail.com>
2024-01-28 17:11:12 +0800

    修改header， 修改友情連結，修改CSS

--- src/components/friend.jsx
+++ src/components/friend.jsx
@@ -6,19 +6,16 @@ const Friend = () => {
 
         <h1>友情連結</h1>
 
-        <h2>自己的部落格</h2>
         <div id="Yun">
-        <p><a href="https://error19137.github.io/">Yun 的個人部落格</a></p>
+        <p>自己的部落格：<a href="https://error19137.github.io/">Yun 的個人部落格</a></p>
         </div>
 
-        <h2>高中朋友</h2>
         <div id="Joseph">
-        <p><a href="https://error0x03f7.vercel.app/?fbclid=IwAR0bc9Hq2MJfFtumDweImh8uoy79arZPSIucxfG3pcrJCjRdLt3U-xVCZwc">Error_0x03F7 個人網站</a></p>
+        <p>高中朋友：<a href="https://error0x03f7.vercel.app/?fbclid=IwAR0bc9Hq2MJfFtumDweImh8uoy79arZPSIucxfG3pcrJCjRdLt3U-xVCZwc">Error_0x03F7 個人網站</a></p>
         </div>
 
-        <h2>大學裝B仔</h2>
         <div id="sao-x">
-        <p><a href="https://blog.sao-x.com/">唯一のBlog</a></p>
+        <p>大學裝B仔：<a href="https://blog.sao-x.com/">唯一のBlog</a></p>
         </div>
             
         </div>
