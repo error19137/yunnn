5f8a5da58cd3543ee92225bee6b32771bcdb8cae
error19137 <thomas.cheng0217@gmail.com>
2024-01-30 01:21:11 +0800

    增加 react 標籤旋轉

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,6 +2,7 @@ const Home = () => {
     return (
         <div id="home" className="page">
             <div id="home-img"></div>
+            <div id="react"><img src="/img/react.svg" alt="React" /></div>
         </div>
     );
 }
