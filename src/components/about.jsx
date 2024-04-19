f35ad0934db64518c5d827a2eddd0e630ccc0055
error19137 <thomas.cheng0217@gmail.com>
2024-04-19 10:16:02 +0800

    增加證照

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,7 +4,7 @@ const About = () => {
 
                 <div>關於我</div>
                 <div>個人簡介</div>
-                <div>我叫Yun ，目前大三生，正在為自己的畢業專題感到煩惱，在沒多久就要第二次提報了 T_T</div>
+                <div>我叫Yun ，目前大三生，正在為自己的畢業專題及實習煩惱 T.T</div>
 
                 <div>正在學習的專業技能</div>
                 <ul>
@@ -14,6 +14,62 @@ const About = () => {
                     <li>幹話技術繼續精進</li>
                     <li>網頁美化技術</li>
                 </ul>
+
+                <table>
+                    <thead>
+                    <tr>
+                    <th>證照名稱</th>
+                    <th>發證日期</th>
+                    <th>發證單位</th>
+                    <th>認證級別</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                    <td>工業電子</td>
+                    <td>Aug 6, 2019</td>
+                    <td>勞動部</td>
+                    <td>丙級</td>
+                    </tr>
+                    <tr>
+                    <td>Adobe Illustrator CC</td>
+                    <td>Dec 22, 2021</td>
+                    <td>Silicon Stone</td>
+                    <td>專業級</td>
+                    </tr>
+                    <tr>
+                    <td>TQC 大型資料庫管理系統 MySQL</td>
+                    <td>Dec 14, 2022</td>
+                    <td>財團法人中華民國電腦技能基金會</td>
+                    <td>專業級</td>
+                    </tr>
+                    <tr>
+                    <td>MTA-資訊技術專業工程師（ITProfessionals）-網路管理與應用工程師核心能力(Networking Fundamentals) MTA98-366</td>
+                    <td>May 11, 2022</td>
+                    <td>微軟(Microsoft)</td>
+                    <td>專業級</td>
+                    </tr>
+                    <tr>
+                    <td>MTA-資訊技術專業工程師（ITProfessionals）-網路安全管理師核心能力(Security Fundamentals) MTA98-367</td>
+                    <td>May 11, 2022</td>
+                    <td>微軟(Microsoft)</td>
+                    <td>專業級</td>
+                    </tr>
+                    <tr>
+                    <td>ITS – 程式語言核心能力 Python</td>
+                    <td>May 10, 2023</td>
+                    <td>資訊產業策進會(Institute for Information Industry)</td>
+                    <td>專業級</td>
+                    </tr>
+                    <tr>
+                    <td>TQC+ 物件導向程式語言 Java</td>
+                    <td>Apr 24, 2023</td>
+                    <td>財團法人中華民國電腦技能基金會</td>
+                    <td>專業級</td>
+                    </tr>
+                    </tbody>
+                    </table>
+
 
                 <div>學業經歷</div>
                 <div>石牌國中（2015/9 ~ 2018/6）</div>
